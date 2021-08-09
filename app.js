452147f3a1c0655605ae57ea3603e20ebe568602
mayoimayoi <tky529@gmail.com>
2021-08-09 22:56:35 +0900

    test3

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 "use strict";
 const express = require("express");
 const app = express();
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 let path = require("path");
 
-require("dotenv").config();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
-
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
